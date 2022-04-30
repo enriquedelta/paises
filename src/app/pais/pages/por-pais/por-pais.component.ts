f3fc9f6d24d986c157d6f09c2b06ffa6c19db647
Enrique <enrique@deltasoftinformatica.com>
2022-04-30 12:39:04 +0200

    Enrique 30-04  1

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [`
+    li {
+      cursor:pointer;
+    }
+  `]
+})
+export class PorPaisComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises : Country[] = [];
+
+  paisesSugeridos : Country[] = [];
+  mostrarSugerencias: boolean = false;
+
+  constructor( private paisService: PaisService) { }
+
+  buscar( termino:string ) {
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = false;
+
+    this.paisService.buscarPais( this.termino )
+      .subscribe({
+        next: (paises) => {
+          console.log( paises );
+          this.paises = paises;
+        },
+        error: (err) => {
+          this.hayError = true;
+          this.paises = [];
+        }
+      });
+  }
+
+  sugerencias( termino: string ) {
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    
+    this.paisService.buscarPais( termino )
+      .subscribe({ 
+        next: (paises) => this.paisesSugeridos = paises.splice(0,5),
+        error:  (err) => this.paisesSugeridos = [] 
+      });
+  }
+
+  buscarSugerido( termino: string ) {
+    this.buscar( termino );
+    
+  }
+
+}
